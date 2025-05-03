97793ad749ffa94a934f58eb7983370fdf92423f
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-05-03 10:49:41 -0400

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,14 +1,7 @@
-package.path = "./lib/?/init.lua;" .. package.path
-
-local modules = {
-  "core",
+local library, modules = ("lib"),
+{ "core",
   "ffi",
   "tools" }
-
 for _, name in ipairs(modules) do
-  local ok, result = pcall(require, name)
-  if type(result) == "function" 
-    then
-      result()
-  end
+  require(library.."."..name)()
 end
