1ecdd5b08429f9236b487b3271375b0d65d4c20a
iemptybrass <187961073+iemptybrass@users.noreply.github.com>
2025-05-03 09:40:48 -0400

    Update init.lua

--- lib/core/import/init.lua
+++ lib/core/import/init.lua
@@ -1,7 +1,7 @@
 return function(...)
   local prefix = debug.getinfo(2, "S").source
-    :match("^@%./(.-)/?[^/]*%..+$")
-    :gsub("/", ".") .. "."
+    :gsub("^@%./(.-/?)[^/]+%..+$", function(input)
+    return (input):gsub("/", ".") end)
   for _, name in ipairs({...}) do
     rawset(getfenv(2), name, require(prefix .. name))
   end
